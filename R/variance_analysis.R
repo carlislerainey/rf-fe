6409cee2767af10bf46ebc9213737ef7e4bc7d23
Florian M. Hollenbach <florian.hollenbach@gmail.com>
2015-08-27 09:59:01 -0500

    var plots, standardized

--- R/variance_analysis.R
+++ R/variance_analysis.R
@@ -1,11 +1,10 @@
 # setwd("~/Dropbox/projects/rf-fe")
-setwd("~/Documents/GitHub/rf-fe/")
 # clear workspace
 rm(list = ls())
 
 # set seed 
 set.seed(89317420)
-
+setwd("~/Documents/GitHub/rf-fe/")
 # load packages
 library(readr)
 library(randomForest)
@@ -13,6 +12,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(plyr)
+library(foreach)
 # load and listwise delete data
 hj <- read_csv("data/hill-jones.csv")
 hj$case_id <- 1:nrow(hj)
@@ -27,12 +27,12 @@ hj <- hj %>% select(-one_of("latent_lag", "physint_lag", "amnesty_lag", "year",
 hj <- hj %>% mutate(gdppc = log(gdppc), pop = log(pop)) %>% as.data.frame(.)
 hj <- na.omit(hj)
 glimpse(hj)
-
-
+hj = data.frame(hj[,1],scale(hj[,-1]))
+names(hj)[1] = "ccode"
 ### create with and between country variance
 
 # create country means & add to dataframe
-cmeans = ddply(hj,.variables = "ccode",numcolwise(mean))
+cmeans = ddply(hj[,-which(names(hj)%in%c("case_id"))],.variables = "ccode",numcolwise(mean))
 glimpse(cmeans)
 numeric_vars = names(cmeans)
 names(cmeans) = paste(names(cmeans),"cmean",sep="_")
@@ -50,9 +50,41 @@ names(demeaned) = paste(numeric_vars[-1],"demeaned",sep="_")
 
 hj = data.frame(hj,demeaned)
 
-btw_variances = data.frame(foreach(i = 2:length(numeric_vars),.combine = cbind)%do%(var(cmeans[,i])))
-names(btw_variances) = numeric_vars[-1]
+btw_variances = data.frame(foreach(i = 2:length(numeric_vars),.combine = rbind)%do%(var(cmeans[,i])))
+row.names(btw_variances) = numeric_vars[-1]
 
-within_variances = data.frame(foreach(i = 2:length(numeric_vars),.combine = cbind)%do%(var(hj[,numeric_vars[i]])))
-names(within_variances) = numeric_vars[-1]
+within_variances = data.frame(foreach(i = 2:length(numeric_vars),.combine = rbind)%do%(var(hj[,paste(numeric_vars[i],"demeaned",sep="_")])))
+row.names(within_variances) = numeric_vars[-1]
 
+
+variances = data.frame(btw_variances,within_variances,btw_variances/within_variances,factor(numeric_vars[-1]))
+names(variances) = c("btw","within","ratio","vars")
+variances$ratio[variances$vars == "britcol" | variances$vars == "common"] = NA
+
+variances$vars1 = reorder(variances$vars, variances$ratio)
+p = ggplot(data = variances)
+p = p + geom_bar(aes(x = vars1,y = ratio),stat= "identity")
+p = p + coord_flip() + theme_bw() 
+p = p + labs(title = "", y="Ratio Betw Within Variance", x="Variables")+ theme(axis.text.x=element_text(size=12.5)) + theme(axis.text.y=element_text(size=15))+ theme(axis.title.x = element_text(size = 20),axis.title.y = element_text(size = 20)) 
+plot(p)
+ggsave("~/Desktop/Btw_With_Ratio.pdf", width = 16, height = 16)
+
+
+
+
+variances$vars2 = reorder(variances$vars, variances$btw)
+p = ggplot(data = variances)
+p = p + geom_bar(aes(x = vars2,y = btw),stat= "identity")
+p = p + coord_flip() + theme_bw() 
+p = p + labs(title = "", y="Between Variance", x="Variables")+ theme(axis.text.x=element_text(size=12.5)) + theme(axis.text.y=element_text(size=15))+ theme(axis.title.x = element_text(size = 20),axis.title.y = element_text(size = 20)) 
+plot(p)
+ggsave("~/Desktop/Btw_Var.pdf", width = 16, height = 16)
+
+
+variances$vars3 = reorder(variances$vars, variances$within)
+p = ggplot(data = variances)
+p = p + geom_bar(aes(x = vars3,y = within),stat= "identity")
+p = p + coord_flip() + theme_bw() 
+p = p + labs(title = "", y="Within Variance", x="Variables")+ theme(axis.text.x=element_text(size=12.5)) + theme(axis.text.y=element_text(size=15))+ theme(axis.title.x = element_text(size = 20),axis.title.y = element_text(size = 20)) 
+plot(p)
+ggsave("~/Desktop/Within_Var.pdf", width = 16, height = 16)
